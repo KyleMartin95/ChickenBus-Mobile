17defeb46c114b61d1827dcced2659f061b11636
KyleMartin95 <kylemartin95@gmail.com>
2017-11-07 13:43:59 -0500

    fixed capitalization errors

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import Expo from 'expo';
-import { Root } from './App/Router';
+import { Root } from './App/router';
 
 export default class App extends React.Component {
   render() {
